a13e6469a44d54c06f3e15df1017ce538aa5c4af
sql-fan <166389859+sql-fan@users.noreply.github.com>
2025-02-26 18:06:21 +0200

    Homepage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,24 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
+
   def new
+    @post = Post.new
   end
+
+  def show
+    
+  end
+
+  def create
+    @post = Post.create(post_params)
+
+    redirect_to @post
+  end
+
+  private
+    def post_params
+      params.expect(post: [ :caption, :image ])
+    end
 end
