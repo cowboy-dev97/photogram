ecbe593f7109f4f248f3be82db30c31517763b1a
sql-fan <166389859+sql-fan@users.noreply.github.com>
2025-02-26 20:04:19 +0200

    add, show, edit post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [ :show, :edit, :update ]
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def new
@@ -8,17 +9,34 @@ class PostsController < ApplicationController
   end
 
   def show
-    
   end
 
   def create
-    @post = Post.create(post_params)
+    @post = Post.new
+    if @post.create(post_params)
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
-    redirect_to @post
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
-    def post_params
-      params.expect(post: [ :caption, :image ])
-    end
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.expect(post: [ :caption, :image ])
+  end
 end
